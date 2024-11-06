f867a821a2da06e4c0e3ff43fefee3943c5e07e1
MartinAgarcia1710 <martindlcg@gmail.com>
2024-11-06 17:09:31 -0300

    clientes. dominio incompleto, a la espera de clase usuaro

--- Grupo_3_TP_Integrador/src/dominio/Cliente.java
+++ Grupo_3_TP_Integrador/src/dominio/Cliente.java
@@ -2,7 +2,16 @@ package dominio;
 
 public class Cliente {
 	public String nombre;
-
+	public String apellido;
+	//En la tabla de la BD el genero es char, me parecio mejor en la clase poner String y cuando trae la info de la BD
+	//comprobar que char trae y completarlo a String para mostrar... Les parece? si no les va cambio a char
+	public String genero;
+	public String correoElectronico;
+	public Localidad localidad;
+	public Provincia provincia;
+	public TipoUsuario tipoUsuario;
+	
+	
 	public String getNombre() {
 		return nombre;
 	}
