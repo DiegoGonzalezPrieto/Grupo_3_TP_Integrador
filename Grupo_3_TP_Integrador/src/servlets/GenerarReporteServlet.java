157980239c0b3b50c792ad5c293435ac3372549d
Diego Prieto <diego_prieto@itrsa.com.ar>
2024-11-12 19:29:39 -0300

    reporte de cuentas (muy basico)

--- Grupo_3_TP_Integrador/src/servlets/GenerarReporteServlet.java
+++ Grupo_3_TP_Integrador/src/servlets/GenerarReporteServlet.java
@@ -2,6 +2,7 @@ package servlets;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.math.BigDecimal;
 import java.nio.charset.Charset;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import javax.servlet.ServletException;
@@ -18,10 +20,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import dominio.Cliente;
+import dominio.Cuenta;
 import dominio.Nacionalidad;
 import dominio.Provincia;
 import negocio.ClienteNegocio;
+import negocio.CuentaNegocio;
 import negocioImpl.ClienteNegocioImpl;
+import negocioImpl.CuentaNegocioImpl;
 import negocioImpl.NacionalidadNegocioImpl;
 import negocioImpl.ProvinciaNegocioImpl;
 
@@ -46,8 +51,7 @@ public class GenerarReporteServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("Reportes.jsp").forward(request, response);
 	}
 
 	/**
@@ -61,9 +65,27 @@ public class GenerarReporteServlet extends HttpServlet {
 		String reporte = "";
 		String nombreReporte = "reporte";
 		String fecha = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+
 		if (tipoReporte.equals("clientes")) {
+
 			reporte = generarReporteClientes();
 			nombreReporte = "reporte_clientes_" + fecha + ".txt";
+
+		} else if (tipoReporte.equals("cuentas")) {
+
+			String[] fragmentosFechaInicio = request.getParameter("fechaInicio").split("-");
+			String[] fragmentosFechaFin = request.getParameter("fechaFin").split("-");
+			if (fragmentosFechaInicio.length != 3 || fragmentosFechaFin.length != 3)
+				return;
+			Date fechaInicio = new Date(Integer.parseInt(fragmentosFechaInicio[0]) - 1900,
+					Integer.parseInt(fragmentosFechaInicio[1]) - 1, Integer.parseInt(fragmentosFechaInicio[2]));
+			Date fechaFin = new Date(Integer.parseInt(fragmentosFechaFin[0]) - 1900,
+					Integer.parseInt(fragmentosFechaFin[1]) - 1, Integer.parseInt(fragmentosFechaFin[2]));
+			reporte = generarReporteCuentas(fechaInicio, fechaFin);
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			String periodo = sdf.format(fechaInicio) + " a " + sdf.format(fechaFin);
+			nombreReporte = "reporte_cuentas_" + periodo + ".txt";
+
 		}
 
 		response.setContentType("text/plain");
@@ -71,6 +93,31 @@ public class GenerarReporteServlet extends HttpServlet {
 		try (OutputStream out = response.getOutputStream()) {
 			out.write(reporte.getBytes());
 		}
+	}
+
+	private String generarReporteCuentas(Date fechaInicio, Date fechaFin) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String periodo = sdf.format(fechaInicio) + "_" + sdf.format(fechaFin);
+		String reporte = "Reporte de Cuentas " + periodo + "\n";
+		reporte += "========================================\n\n";
+
+		CuentaNegocio n = new CuentaNegocioImpl();
+		List<Cuenta> cuentasTodas = n.listarTodas();
+		List<Cuenta> cuentasPeriodo = n.listarTodas();
+		cuentasPeriodo.removeIf(c -> new Date(c.getFechaCreacion().getTime()).before(fechaInicio)
+				|| c.getFechaCreacion().after(fechaFin));
+
+		BigDecimal totalSaldo = new BigDecimal(0);
+		for (Cuenta cuenta : cuentasPeriodo) {
+			totalSaldo = totalSaldo.add(cuenta.getSaldo());
+		}
+
+		reporte += "- Cantidad de Cuentas creadas durante el período: " + cuentasPeriodo.size() + "\n\n";
+		reporte += "- Suma de saldos de Cuentas creadas durante el período: $" + totalSaldo + "\n";
+		reporte += "- Promedio de saldos de Cuentas creadas durante el período: $"
+				+ totalSaldo.divide(new BigDecimal(cuentasPeriodo.size())) + "\n";
+
+		return reporte;
 	}
 
 	private String generarReporteClientes() {
@@ -128,7 +175,7 @@ public class GenerarReporteServlet extends HttpServlet {
 			}
 			if (contador > 0) {
 				porcentajesNacionalidad += "\t* " + nacionalidad.getNombre() + " - "
-						+ df.format(((float)contador * 100.00 / cantidadActivos) )+ "%\n";
+						+ df.format(((float) contador * 100.00 / cantidadActivos)) + "%\n";
 			}
 		}
 
