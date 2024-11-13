2a9013f43a8af3bab98d7a5f787c68477edbd30a
FlorVarangot <majoreyes87@gmail.com>
2024-11-13 13:31:30 -0300

    Agrego Exceptions usuario y pass a LogIn

---
+++ Grupo_3_TP_Integrador/src/exceptions/UsuarioNoEncontradoException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class UsuarioNoEncontradoException extends Exception{
+
+	private static final long serialVersionUID = 1L; 
+	
+	public UsuarioNoEncontradoException(){ 
+		super("El usuario no existe."); 
+	}
+}
+
